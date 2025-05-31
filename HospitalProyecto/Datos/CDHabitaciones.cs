d0ee5e745e74af9735885a3f355b322d4677d37d
Emmanuel <User@LAPTOP-UDFJVI5J>
2025-05-30 22:22:39 -0600

    copia

---
+++ HospitalProyecto/Datos/CDHabitaciones.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace HospitalProyecto.Datos
+{
+    internal class CDHabitaciones
+    {
+        CDconexión cd_conexion = new CDconexión();
+
+        public DataTable MtdConsultarHabitaciones()
+        {
+            string QueryConsultar = "Select * from tbl_Habitaciones";
+            SqlDataAdapter SqlAdap = new SqlDataAdapter(QueryConsultar, cd_conexion.MtdAbrirConexion());
+            DataTable Dt = new DataTable();
+            SqlAdap.Fill(Dt);
+            cd_conexion.MtdCerrarConexion();
+            return Dt;
+        }
+
+        public void MtdAgregarHabitaciones(string Numero, string Ubicacion, string TipoHabitacion, double Costo, string Estado, string UsuarioAuditoria, DateTime FechaAuditoria)
+        {
+            string QueryAgregar = "Insert into tbl_Habitaciones (Numero, Ubicacion, TipoHabitacion, Costo, Estado, UsuarioAuditoria, FechaAuditoria) values (@Numero, @Ubicacion, @TipoHabitacion, @Costo, @Estado, @UsuarioAuditoria, @FechaAuditoria)";
+            SqlCommand cmd = new SqlCommand(QueryAgregar, cd_conexion.MtdAbrirConexion());
+            cmd.Parameters.AddWithValue("@Numero", Numero);
+            cmd.Parameters.AddWithValue("@Ubicacion", Ubicacion);
+            cmd.Parameters.AddWithValue("@TipoHabitacion", TipoHabitacion);
+            cmd.Parameters.AddWithValue("@Costo", Costo);
+            cmd.Parameters.AddWithValue("@Estado", Estado);
+            cmd.Parameters.AddWithValue("@UsuarioAuditoria", UsuarioAuditoria);
+            cmd.Parameters.AddWithValue("@FechaAuditoria", FechaAuditoria);
+            cmd.ExecuteNonQuery();
+            cd_conexion.MtdCerrarConexion();
+        }
+
+        public void MtdActualizarHabitaciones(int CodigoHabitacion, string Numero, string Ubicacion, string TipoHabitacion, double Costo, string Estado, string UsuarioAuditoria, DateTime FechaAuditoria)
+        {
+            string QueryActualizar = "Insert into tbl_Habitaciones (CodigoHabitacion, Numero, Ubicacion, TipoHabitacion, Costo, Estado, UsuarioAuditoria, FechaAuditoria ) values (@CodigoHabitacion, @Numero, @Ubicacion, @TipoHabitacion, @Costo, @Estado, @UsuarioAuditoria, @FechaAuditoria)";
+            SqlCommand cmd = new SqlCommand(QueryActualizar, cd_conexion.MtdAbrirConexion());
+            cmd.Parameters.AddWithValue("@CodigoHabitacion", CodigoHabitacion);
+            cmd.Parameters.AddWithValue("@Numero", Numero);
+            cmd.Parameters.AddWithValue("@Ubicacion", Ubicacion);
+            cmd.Parameters.AddWithValue("@TipoHabitacion", TipoHabitacion);
+            cmd.Parameters.AddWithValue("@Costo", Costo);
+            cmd.Parameters.AddWithValue("@Estado", Estado);
+            cmd.Parameters.AddWithValue("@UsuarioAuditoria", UsuarioAuditoria);
+            cmd.Parameters.AddWithValue("@FechaAuditoria", FechaAuditoria);
+            cmd.ExecuteNonQuery();
+            cd_conexion.MtdCerrarConexion();
+        }
+
+        public void MtdEliminarHabitaciones(int CodigoHabitacion)
+        {
+            string QueryEliminar = "Delete from tbl_Habitaciones where CodigoHabitacion=@CodigoHabitacion";
+            SqlCommand cmd = new SqlCommand(QueryEliminar, cd_conexion.MtdAbrirConexion());
+            cmd.Parameters.AddWithValue("@CodigoHabitacion", CodigoHabitacion);
+            cmd.ExecuteNonQuery();
+            cd_conexion.MtdCerrarConexion();
+        }
+    }
+}
---
+++ HospitalProyecto/Presentación/FrmGestionHabitaciones.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace HospitalProyecto
+{
+    partial class GestionHabitaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCodigoGestion = new System.Windows.Forms.TextBox();
+            this.dtpFechaGestion = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtCodigoEmpleado = new System.Windows.Forms.TextBox();
+            this.lblFechaAuditoria = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtCodigoHabitacion = new System.Windows.Forms.TextBox();
+            this.txtUsuarioAuditoria = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cboxTipoGestion = new System.Windows.Forms.ComboBox();
+            this.cboxEstado = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calisto MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(269, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 24);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "GESTIÓN HABITACIONES";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.txtCodigoGestion);
+            this.groupBox1.Controls.Add(this.dtpFechaGestion);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.btnEditar);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.txtCodigoEmpleado);
+            this.groupBox1.Controls.Add(this.lblFechaAuditoria);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.txtCodigoHabitacion);
+            this.groupBox1.Controls.Add(this.txtUsuarioAuditoria);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.cboxTipoGestion);
+            this.groupBox1.Controls.Add(this.cboxEstado);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Location = new System.Drawing.Point(32, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(737, 153);
+            this.groupBox1.TabIndex = 33;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtCodigoGestion
+            // 
+            this.txtCodigoGestion.Location = new System.Drawing.Point(128, 11);
+            this.txtCodigoGestion.Name = "txtCodigoGestion";
+            this.txtCodigoGestion.ReadOnly = true;
+            this.txtCodigoGestion.Size = new System.Drawing.Size(121, 20);
+            this.txtCodigoGestion.TabIndex = 40;
+            // 
+            // dtpFechaGestion
+            // 
+            this.dtpFechaGestion.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaGestion.Location = new System.Drawing.Point(428, 11);
+            this.dtpFechaGestion.Name = "dtpFechaGestion";
+            this.dtpFechaGestion.Size = new System.Drawing.Size(121, 20);
+            this.dtpFechaGestion.TabIndex = 39;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(314, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 15);
+            this.label6.TabIndex = 38;
+            this.label6.Text = "Fecha gestión:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Codigo gestión:";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(610, 105);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 31;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(610, 58);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 30;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 15);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Codigo empleado:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(610, 12);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 29;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtCodigoEmpleado
+            // 
+            this.txtCodigoEmpleado.Location = new System.Drawing.Point(128, 41);
+            this.txtCodigoEmpleado.Name = "txtCodigoEmpleado";
+            this.txtCodigoEmpleado.Size = new System.Drawing.Size(121, 20);
+            this.txtCodigoEmpleado.TabIndex = 16;
+            // 
+            // lblFechaAuditoria
+            // 
+            this.lblFechaAuditoria.AutoSize = true;
+            this.lblFechaAuditoria.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaAuditoria.Location = new System.Drawing.Point(425, 113);
+            this.lblFechaAuditoria.Name = "lblFechaAuditoria";
+            this.lblFechaAuditoria.Size = new System.Drawing.Size(99, 15);
+            this.lblFechaAuditoria.TabIndex = 28;
+            this.lblFechaAuditoria.Text = "Imprimir Fecha:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(10, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 15);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Codigo habitacion:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(314, 113);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(97, 15);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "Fecha auditoria:";
+            // 
+            // txtCodigoHabitacion
+            // 
+            this.txtCodigoHabitacion.Location = new System.Drawing.Point(128, 75);
+            this.txtCodigoHabitacion.Name = "txtCodigoHabitacion";
+            this.txtCodigoHabitacion.Size = new System.Drawing.Size(121, 20);
+            this.txtCodigoHabitacion.TabIndex = 18;
+            // 
+            // txtUsuarioAuditoria
+            // 
+            this.txtUsuarioAuditoria.Location = new System.Drawing.Point(428, 75);
+            this.txtUsuarioAuditoria.Name = "txtUsuarioAuditoria";
+            this.txtUsuarioAuditoria.Size = new System.Drawing.Size(121, 20);
+            this.txtUsuarioAuditoria.TabIndex = 26;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(10, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 15);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Tipo gestión:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(314, 80);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(108, 15);
+            this.label9.TabIndex = 25;
+            this.label9.Text = "Usuario auditoria:";
+            // 
+            // cboxTipoGestion
+            // 
+            this.cboxTipoGestion.FormattingEnabled = true;
+            this.cboxTipoGestion.Items.AddRange(new object[] {
+            "Preparación",
+            "Limpieza ",
+            "Observación",
+            "Alimentación"});
+            this.cboxTipoGestion.Location = new System.Drawing.Point(128, 107);
+            this.cboxTipoGestion.Name = "cboxTipoGestion";
+            this.cboxTipoGestion.Size = new System.Drawing.Size(121, 21);
+            this.cboxTipoGestion.TabIndex = 20;
+            // 
+            // cboxEstado
+            // 
+            this.cboxEstado.FormattingEnabled = true;
+            this.cboxEstado.Items.AddRange(new object[] {
+            "Disponible",
+            "Preparación",
+            "Ocupada",
+            "Bloqueada"});
+            this.cboxEstado.Location = new System.Drawing.Point(428, 39);
+            this.cboxEstado.Name = "cboxEstado";
+            this.cboxEstado.Size = new System.Drawing.Size(121, 21);
+            this.cboxEstado.TabIndex = 24;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(314, 46);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 15);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Estado:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 205);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(737, 192);
+            this.dataGridView1.TabIndex = 35;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(552, 403);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 36;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(642, 403);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 37;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // GestionHabitaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 433);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "GestionHabitaciones";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtCodigoEmpleado;
+        private System.Windows.Forms.Label lblFechaAuditoria;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtCodigoHabitacion;
+        private System.Windows.Forms.TextBox txtUsuarioAuditoria;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cboxTipoGestion;
+        private System.Windows.Forms.ComboBox cboxEstado;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.DateTimePicker dtpFechaGestion;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCodigoGestion;
+    }
+}
